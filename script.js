fcaa6578b77a7ccb70f384eceecd069d29776289
HinaZeeshan <147984993+HinaZeeshan@users.noreply.github.com>
2024-11-19 20:30:51 +0500

    html css based website

---
+++ script.js
@@ -0,0 +1,10 @@
+function showslidebar(){
+  const slidebar = document.querySelector('.slidebar')
+   slidebar.style.display = 'flex'
+}
+function hideslidebar(){
+  const slidebar = document.querySelector('.slidebar')
+  slidebar.style.display = 'none'
+}
+
+  
